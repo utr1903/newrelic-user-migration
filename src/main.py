bad826eecb7720c4c33fb25d83764c4ae08b1e9c
uturkarslan <ugurturkarslan@gmail.com>
2023-01-03 19:25:19 +0100

    Implement pagination

--- src/main.py
+++ src/main.py
@@ -71,53 +71,68 @@ def main():
     print('New Relic region can be either us or eu!')
     return
 
-  #######################
-  ### Execute request ###
-  #######################
+  ###########################################
+  ### Fetch users from the source account ###
+  ###########################################
   headers = {
     "Api-Key": srcApiKey,
     "Content-Type": 'application/json'
   }
 
+  allUsers = []
   nextCursor = None
-  queryTemplate = Template("""
-  {
-    actor {
-      users {
-        userSearch(
-          cursor: $nextCursor
-        ) {
-          users {
-            name
-            email
-            userId
+
+  while True:
+    queryTemplate = Template("""
+    {
+      actor {
+        users {
+          userSearch(
+            cursor: $nextCursor
+          ) {
+            users {
+              name
+              email
+              userId
+            }
+            nextCursor
           }
-          nextCursor
         }
       }
     }
-  }
-  """)
+    """)
 
-  # Substitute variables
-  query = queryTemplate.substitute(
-    nextCursor = "null" if nextCursor == None else nextCursor,
-  )
+    # Substitute variables
+    query = queryTemplate.substitute(
+      nextCursor = "null" if nextCursor == None else nextCursor,
+    )
 
-  # Execute request
-  request = requests.post(
-    'https://api.eu.newrelic.com/graphql' if tgtRegion == "eu" else 'https://api.newrelic.com/graphql',
-    json={'query': query},
-    headers=headers
-  )
+    # Execute request
+    request = requests.post(
+      'https://api.eu.newrelic.com/graphql' if tgtRegion == "eu" else 'https://api.newrelic.com/graphql',
+      json={'query': query},
+      headers=headers
+    )
 
-  if request.status_code != 200:
-    print("Query failed to run by returning code of {}. {}".format(request.status_code, query))
-    return
+    if request.status_code != 200:
+      print("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+      return
 
-  result = request.json()
+    result = request.json()
+    for user in result["data"]["actor"]["users"]["userSearch"]["users"]:
+      allUsers.append(user)
 
-  allUsers = result["data"]["actor"]["users"]["userSearch"]["users"]
+    # Continue to parse till all the data is fetched
+    nextCursor = result["data"]["actor"]["users"]["userSearch"]["nextCursor"]
+    if nextCursor == None:
+      print("All users are fetched.")
+      break
+    else:
+      print("Users are not fetched entirely. Continuing...")
+
+  ##############################################
+  ### Create new users in the target account ###
+  ##############################################
   for user in allUsers:
     print(user)
 
